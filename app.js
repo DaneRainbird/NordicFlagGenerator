b4adcb4d7bac9b3324e092e6fea0e991b0219bde
datenyan <13450157+Datenyan@users.noreply.github.com>
2021-10-31 14:30:18 +1100

    Added basic routing

--- app.js
+++ app.js
@@ -7,11 +7,21 @@
  /** DEPENDENCIES **/
 let express = require('express');
 
+/** EXPRESS CONFIGURATION **/
 let app = express();
 let port = process.env.PORT || 8080;
+
+/** RENDER ENGINE **/
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 /** STATIC ROUTING CONFIGURATION **/
 app.use(express.static(__dirname + '/res'));
 
 /** RUN THE SERVER s**/
 app.listen(port, () => console.log("Listening on port " + port))
+
+/** ROUTING **/
+app.get('/', (req, res) => {
+    res.render("index.html");
+})
